ad340d43288739eb30222ca4a2d05234eef1b651
Julio Sanquis <jsanquis@gmail.com>
2014-09-18 18:40:37 +0200

    Validación de Formularios

---
+++ js/jquery_form.js
@@ -0,0 +1,51 @@
+$(document).on('ready', function(){
+
+	var emailreg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+
+	$('#submit').on('click', function(){
+
+		$(".error").remove();
+
+		if( $('#nombre').val() == "" ){
+			$('#nombre').focus().after("<span class='error'>Olvidaste colocar tu nombre</span>");
+			return false;
+		} else if( $('#telefono').val().length < 1 || isNaN( $('#telefono').val() ) ||  $('#telefono').val().length < 9){
+			$('#telefono').focus().after("<span class='error'>Ingrese un teléfono</span>");
+			return false;
+		} else if( $('#email').val() == "" || !emailreg.test( $('#email').val() ) ){
+			$('#email').focus().after("<span class='error'>Ingrese un email válido</span>");
+			return false;
+		} else {
+			alert("Sus datos han sido enviados correctamente");
+		}
+	});
+
+	$('#nombre').keyup(function(){
+		if( $(this).val() != "" ){
+			$('.error').fadeOut();			
+			return false;
+		}		
+	});
+
+	$('#telefono').keyup(function(){
+		if( isNaN( $(this).val() ) || $(this).val().length == 9 ){
+			$('.error').fadeOut();			
+			return false;
+		}		
+	});
+
+	$('#email').keyup(function(){
+		if( $(this).val() != "" && emailreg.test($(this).val())){
+			$('.error').fadeOut();			
+			return false;
+		}		
+	});
+
+
+
+
+
+
+
+
+});
