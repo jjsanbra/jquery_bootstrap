de8a3ab4d58745c27312e1337499fb18c8888232
Julio Sanquis <jsanquis@gmail.com>
2014-09-24 17:57:25 +0200

    wip - Ventana Modal Dinámica

--- js/jquery_plugins.js
+++ js/jquery_plugins.js
@@ -7,15 +7,20 @@ $(document).on('ready', function(){
 	$('#contenedor').mixItUp();
 
 
-
 	/*
 	--- Inicialización de Plugin de Efectos MouseOver sobre Imágenes
 		Plugin: Adipoli - Sitio Web: https://cube3x.com/adipoli-jquery-image-hover-plugin/	
 	*/
-	$('.mix').adipoli({
+	$('article figure img').adipoli({
 	    'startEffect' : 'normal',
 	    'hoverEffect' : 'popout'
 	});
 
+
+	/*
+	--- Inicialización de Plugin de Efecto Lupa
+		Plugin: MagnifyMe - Sitio Web: http://www.codepunker.com/blog/magnify-me-jquery-image-magnifier-plugin	
+	*/
+    jQuery('.magnifyMe').magnifyMe('lupa');
 
 });
